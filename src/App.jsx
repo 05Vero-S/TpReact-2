4621663d528f3bd4342aa84f15f2d6e940760393
Vero <vdsoria05@gmail.com>
2024-02-03 21:04:44 -0300

    agregue el componente escrito como MensajeModificado

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
+import MensajeModificado from "./components/MensajeModificado"
 
 function App() {
   
   return (
     <>
+    <section>
+    <h1>Hello world!</h1>
+    <MensajeModificado/>
+
+    </section>
       
       
     </>
   )
 }
-
+ 
 export default App
---
+++ src/components/MensajeModificado.jsx
@@ -0,0 +1,11 @@
+
+
+const MensajeModificado = () => {
+    return (
+        <article>
+            <h2>Hello</h2>
+        </article>
+    );
+};
+
+export default MensajeModificado;
